f1434860c8d2390c191838270534f241525ea50b
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2020-03-28 23:52:29 +0530

    Updated

--- asessts/js/main.js
+++ asessts/js/main.js
@@ -1,3 +1,36 @@
+//H1 Animation
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({
+        loop: false
+    })
+    .add({
+        targets: '.ml11 .line',
+        scaleY: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700
+    })
+    .add({
+        targets: '.ml11 .line',
+        translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100
+    }).add({
+        targets: '.ml11 .letter',
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: '-=775',
+        delay: (el, i) => 34 * (i + 1)
+    }).add({
+        targets: '.ml11',
+        opacity: 1
+    });
+
+//Navbar-Fixed
 $(function () {
     "use strict";
 
