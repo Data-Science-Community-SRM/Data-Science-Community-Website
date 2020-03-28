5fe97e257121b631e32fae56f0fa1c8a0bf3a731
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2020-03-28 15:49:48 +0530

    homepage

---
+++ asessts/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+    "use strict";
+
+    var navTopOffset = $('header').height() + 50;
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+    function navbarFixed() {
+        if ($('.header-area').length) {
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
+                if (scroll >= navTopOffset) {
+                    $(".header-area").addClass("navbar_fixed");
+                } else {
+                    $(".header-area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+});
