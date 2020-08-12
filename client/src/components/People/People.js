35307bff342cd49304da49c2ca84ba0ddbafbba3
Hritik Bhandari <bhandari.hritik@gmail.com>
2020-08-12 15:37:22 +0530

    team page update

--- client/src/components/People/People.js
+++ client/src/components/People/People.js
@@ -8,7 +8,7 @@ import "./People.css";
 const People = (props) => {
   return (
     <React.Fragment>
-      <div className={"members"} data-aos="zoom-in-down">
+      <div className={"members"} data-aos="zoom-in-up">
         <img
           className={"photo"}
           alt={props.name + " image"}
