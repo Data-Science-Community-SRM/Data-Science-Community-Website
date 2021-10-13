d351d4a601f437cb5f7ec7083557493f0264a1c6
Arun Jangra <arunjngra89@gmail.com>
2021-10-13 19:51:17 +0530

    projects-card-update ✅

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
 import "../Projects/Projects.css";
-import logo1 from "../Projects/logocopy.png";
-import logo2 from "../Projects/git.png";
-import img1 from "../Projects/dsc-projectcover.png";
 import projectsLogo from "../Projects/project-page.svg";
 import { useState, useEffect } from "react";
 
@@ -23,28 +19,18 @@ function Projects() {
   const getProjects = () => {
     let projectCards = [];
     for (let projects of projectData) {
+      console.log(projects);
       projectCards.push(
-        <div>
-          <div className="card-container">
-            <Card className="Card">
-              <Card.Body className="Lower">
-                <Card.Title className="title">{projects.title}</Card.Title>
-                <div className="upper">
-                  <Card.Img
-                    className="project-img"
-                    variant="top"
-                    src={projects.img}
-                  />
-                </div>
-                <p>{projects.description}</p>
-                <div className="logos">
-                  <img className="ico" src={logo1} alt="" />
-                  <a href={projects.link} target="_blank">
-                    <img className="ico" src={logo2} id="git" alt="" />
-                  </a>
-                </div>
-              </Card.Body>
-            </Card>
+        <div className='project__card'>
+          <div className='project__card__left'>
+            <a href={projects.link} target='_blank' rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='#fff' id='svg__card'><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+            </a>
+            <h1>{projects.title}</h1>
+          </div>
+          <div className='project__card__right'>
+            <p>{projects.description}</p>
+            <img src={projects.img} alt='imj'></img>
           </div>
         </div>
       );
@@ -56,7 +42,7 @@ function Projects() {
     <div>
       <div className="maincont">
         <div className="upperbd">
-          <h1 data-aos="zoom-in-up" style={{ marginTop: "80px" }}  className="head">Projects</h1>
+          <h1 data-aos="zoom-in-up" style={{ marginTop: "80px" }} className="head">Projects</h1>
           <p >
             We apply our coding skills to a wide range of datasets to solve
             real-world problems. Here are some inspirational ideas and their
@@ -64,13 +50,15 @@ function Projects() {
           </p>
         </div>
         <div className="sidebd">
-          <img 
+          <img
             src={projectsLogo}
             alt=""
           />
         </div>
       </div>
-      <div className="cardsPlacement">{getProjects()}</div>
+      <div className="card-container">
+        {getProjects()}
+      </div>
     </div>
   );
 }
