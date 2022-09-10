90dec7834f1d7f678bb09d9c5fa13c649585626d
Arun Jangra <arunjngra89@gmail.com>
2022-09-10 18:34:17 +0530

    UPT: peoples page update

--- client/src/pages/People/People.js
+++ client/src/pages/People/People.js
@@ -2,149 +2,151 @@ import React, { Component } from "react";
 
 import PeopleComponent from "../../components/People/People";
 import {
-  presidents,
-  board,
-  advisor,
-  techmembers,
-  designmembers,
-  mgmtmembers,
+    presidents,
+    board,
+    advisor,
+    techmembers,
+    designmembers,
+    mgmtmembers,
 } from "./PeopleList";
 
 class People extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <div className="col-12 col-lg-9 mx-auto">
-          <h1 data-aos="zoom-in-up" style={{ marginTop: "120px" }}>
-            People
-          </h1>
-          <p style={{ margin: "30px auto" }}>
-            The ones who make our community worth it!
-          </p>
-          <br />
-          <div className="row text-center d-block">
-            <h3 style={{ fontWeight: "700" }}>
-              <span style={{ color: "#6F389F" }}>T</span>HE{" "}
-              <span style={{ color: "#6F389F" }}>B</span>OARD.
-            </h3>
-            <br />
-            {presidents.map((president) => (
-              <PeopleComponent
-                key={president.fname}
-                fname={president.fname}
-                lname={president.lname}
-                role={president.role}
-                image={president.image}
-                github={president.github}
-                linkedin={president.linkedin}
-              />
-            ))}
-          </div>
-          <div className="row text-center d-block">
-            {board.map((board) => (
-              <PeopleComponent
-                key={board.fname}
-                fname={board.fname}
-                lname={board.lname}
-                role={board.role}
-                image={board.image}
-                github={board.github}
-                linkedin={board.linkedin}
-              />
-            ))}
-          </div>
-          
-          <hr />
-          <div className="row text-center d-block">
-            <h3 style={{ fontWeight: "700" }}>
-              <span style={{ color: "#6F389F" }}>T</span>ECHNICAL{" "}
-              <span style={{ color: "#6F389F" }}>T</span>EAM.
-            </h3>
-          </div>
-          <br />
-          <div className="row text-center d-block">
-            {techmembers.map((member) => (
-              <PeopleComponent
-                key={member.fname}
-                fname={member.fname}
-                lname={member.lname}
-                role={member.role}
-                image={member.image}
-                github={member.github}
-                linkedin={member.linkedin}
-              />
-            ))}
-          </div>
-          <hr />
-          <div className="row text-center d-block">
-            <h3 style={{ fontWeight: "700" }}>
-              <span style={{ color: "#6F389F" }}>D</span>ESIGN{" "}
-              <span style={{ color: "#6F389F" }}>T</span>EAM.
-            </h3>
-          </div>
-          <br />
-          <div className="row text-center d-block">
-            {designmembers.map((member) => (
-              <PeopleComponent
-                key={member.fname}
-                fname={member.fname}
-                lname={member.lname}
-                role={member.role}
-                image={member.image}
-                github={member.github}
-                linkedin={member.linkedin}
-              />
-            ))}
-          </div>
-          <hr />
-          <div className="row text-center d-block">
-            <h3 style={{ fontWeight: "700" }}>
-              <span style={{ color: "#6F389F" }}>M</span>ANAGEMENT{" "}
-              <span style={{ color: "#6F389F" }}>T</span>EAM.
-            </h3>
-          </div>
-          <br />
-          <div className="row text-center d-block">
-            {mgmtmembers.map((member) => (
-              <PeopleComponent
-                key={member.fname}
-                fname={member.fname}
-                lname={member.lname}
-                role={member.role}
-                image={member.image}
-                github={member.github}
-                linkedin={member.linkedin}
-              />
-            ))}
-          </div>
-          <hr />
-          <div className="row text-center d-block">
-            <h3 style={{ fontWeight: "700" }}>
-              <span style={{ color: "#6F389F" }}>A</span>LUMNI{" "}
-            </h3>
-          </div>
-          <div className="row text-center d-block">
-            {advisor.map((board) => (
-              <PeopleComponent
-                key={board.fname}
-                fname={board.fname}
-                lname={board.lname}
-                role={board.role}
-                image={board.image}
-                github={board.github}
-                linkedin={board.linkedin}
-              />
-            ))}
-          </div>
+    componentDidMount() {
+        window.scrollTo(0, 0);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div className="col-12 col-lg-9 mx-auto">
+                    <h1 data-aos="zoom-in-up" style={{ marginTop: "120px" }}>
+                        People
+                    </h1>
+                    <p style={{ margin: "30px auto" }}>
+                        The ones who make our community worth it! and success
+                    </p>
+                    <br />
+                    <div className="row text-center d-block">
+                        <h3 style={{ fontWeight: "700" }}>
+                            <span style={{ color: "#6F389F" }}>T</span>HE{" "}
+                            <span style={{ color: "#6F389F" }}>B</span>OARD.
+                        </h3>
+                        <br />
+                        {presidents.map((president) => {
+                            return (
+                                <PeopleComponent
+                                    key={president.fname}
+                                    fname={president.fname}
+                                    lname={president.lname}
+                                    role={president.role}
+                                    image={president.image}
+                                    github={president.github}
+                                    linkedin={president.linkedin}
+                                />
+                            )
+                        })}
+                    </div>
+                    <div className="row text-center d-block">
+                        {board.map((board) => (
+                            <PeopleComponent
+                                key={board.fname}
+                                fname={board.fname}
+                                lname={board.lname}
+                                role={board.role}
+                                image={board.image}
+                                github={board.github}
+                                linkedin={board.linkedin}
+                            />
+                        ))}
+                    </div>
 
-        </div>
-        <hr />
-      </React.Fragment>
-    );
-  }
+                    <hr />
+                    <div className="row text-center d-block">
+                        <h3 style={{ fontWeight: "700" }}>
+                            <span style={{ color: "#6F389F" }}>T</span>ECHNICAL{" "}
+                            <span style={{ color: "#6F389F" }}>T</span>EAM.
+                        </h3>
+                    </div>
+                    <br />
+                    <div className="row text-center d-block">
+                        {techmembers.map((member) => (
+                            <PeopleComponent
+                                key={member.fname}
+                                fname={member.fname}
+                                lname={member.lname}
+                                role={member.role}
+                                image={member.image}
+                                github={member.github}
+                                linkedin={member.linkedin}
+                            />
+                        ))}
+                    </div>
+                    <hr />
+                    <div className="row text-center d-block">
+                        <h3 style={{ fontWeight: "700" }}>
+                            <span style={{ color: "#6F389F" }}>D</span>ESIGN{" "}
+                            <span style={{ color: "#6F389F" }}>T</span>EAM.
+                        </h3>
+                    </div>
+                    <br />
+                    <div className="row text-center d-block">
+                        {designmembers.map((member) => (
+                            <PeopleComponent
+                                key={member.fname}
+                                fname={member.fname}
+                                lname={member.lname}
+                                role={member.role}
+                                image={member.image}
+                                github={member.github}
+                                linkedin={member.linkedin}
+                            />
+                        ))}
+                    </div>
+                    <hr />
+                    <div className="row text-center d-block">
+                        <h3 style={{ fontWeight: "700" }}>
+                            <span style={{ color: "#6F389F" }}>M</span>ANAGEMENT{" "}
+                            <span style={{ color: "#6F389F" }}>T</span>EAM.
+                        </h3>
+                    </div>
+                    <br />
+                    <div className="row text-center d-block">
+                        {mgmtmembers.map((member) => (
+                            <PeopleComponent
+                                key={member.fname}
+                                fname={member.fname}
+                                lname={member.lname}
+                                role={member.role}
+                                image={member.image}
+                                github={member.github}
+                                linkedin={member.linkedin}
+                            />
+                        ))}
+                    </div>
+                    <hr />
+                    <div className="row text-center d-block">
+                        <h3 style={{ fontWeight: "700" }}>
+                            <span style={{ color: "#6F389F" }}>A</span>LUMNI{" "}
+                        </h3>
+                    </div>
+                    <div className="row text-center d-block">
+                        {advisor.map((board) => (
+                            <PeopleComponent
+                                key={board.fname}
+                                fname={board.fname}
+                                lname={board.lname}
+                                role={board.role}
+                                image={board.image}
+                                github={board.github}
+                                linkedin={board.linkedin}
+                            />
+                        ))}
+                    </div>
+
+                </div>
+                <hr />
+            </React.Fragment>
+        );
+    }
 }
 
 export default People;
